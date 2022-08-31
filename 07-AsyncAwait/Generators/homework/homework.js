082411cdd99b3a99ada7121bc15a1f0f5cc7462a
Jesús Lautaro Careglio Albornoz <jlcareglioalbornoz@gmail.com>
2022-08-31 19:06:10 -0300

    ✅

--- 07-AsyncAwait/Generators/homework/homework.js
+++ 07-AsyncAwait/Generators/homework/homework.js
@@ -1,6 +1,13 @@
-function fizzBuzzGenerator(max) {
+function* fizzBuzzGenerator(max = 0) {
   // Tu código acá:
-
+  let num = 1;
+  while (num <= max || max === 0) {
+    if (!(num % 3) && !(num % 5)) yield "Fizz Buzz";
+    else if (!(num % 3)) yield "Fizz";
+    else if (!(num % 5)) yield "Buzz";
+    else yield num;
+    num++;
+  }
 }
 
 module.exports = fizzBuzzGenerator;
