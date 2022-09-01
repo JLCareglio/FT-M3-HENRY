0e7c265c2c4bb8e239ff5b17ba9c8f3261b2cef6
Jesús Lautaro Careglio Albornoz <jlcareglioalbornoz@gmail.com>
2022-09-01 09:52:00 -0300

    ✅

--- 07-AsyncAwait/Generators/homework/homework.js
+++ 07-AsyncAwait/Generators/homework/homework.js
@@ -1,7 +1,7 @@
-function* fizzBuzzGenerator(max = 0) {
+function* fizzBuzzGenerator(max = Infinity) {
   // Tu código acá:
   let num = 1;
-  while (num <= max || max === 0) {
+  while (num <= max) {
     if (!(num % 3) && !(num % 5)) yield "Fizz Buzz";
     else if (!(num % 3)) yield "Fizz";
     else if (!(num % 5)) yield "Buzz";
